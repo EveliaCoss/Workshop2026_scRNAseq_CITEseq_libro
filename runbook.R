72bf809e25e56091845de437e8ee39086c83e605
Evelia Coss <y0kocrozz@gmail.com>
2026-06-26 13:01:39 -0600

    agregar boton de lenguajes

---
+++ runbook.R
@@ -0,0 +1,14 @@
+# traducir libros
+babelquarto::quarto_multilingual_book(
+  parent_dir = ".",
+  project_dir = "draft",
+  main_language = "en",
+  further_languages = c("es", "pt")
+)
+
+
+
+babelquarto::render_book(".")
+
+
+
