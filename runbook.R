52b26d5aa060f7e33387e752ba54443f6d3a97a4
Evelia Coss <y0kocrozz@gmail.com>
2026-06-26 13:41:00 -0600

    pagina web con botones

--- runbook.R
+++ runbook.R
@@ -11,6 +11,14 @@ babelquarto::quarto_multilingual_book(
 babelquarto::render_book(".")
 babelquarto::render_book(site_url= "https://eveliacoss.github.io/Workshop2026_scRNAseq_CITEseq_libro/")
  
+
+babelquarto::render_book(
+  project_path = ".",
+  site_url = "https://eveliacoss.github.io/Workshop2026_scRNAseq_CITEseq_libro/",
+  profile = NULL,
+  preview = servr::httw("docs")
+)
+
 # For a multilingual book
 servr::httw("_book")
 servr::httw("docs")
